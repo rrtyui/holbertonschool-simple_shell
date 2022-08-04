a775e01cc5ac36c3945ee5cfbade1d7b95cf3485
rrtyui <marcelo6549@gmail.com>
2022-08-03 20:50:09 -0700

    fixedshell

---
+++ stokeni.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * stoken - function to tokenize certain buffer
+ * @buffer: the buffer,lol
+ * Return: argv val
+ */
+char **stoken(char *buffer)
+{
+	char **argk;
+	char *tokeni;
+	int i;
+
+	tokeni = strtok(buffer, " \n\t");
+
+	if(tokeni == NULL)
+		return (NULL);
+
+	argk = calloc(1024, sizeof(char *));
+	if (!argk)
+		return (NULL);
+
+	for(i = 0; i < 1024 && tokeni != NULL; i++)
+	{
+		argk[i] = tokeni;
+		tokeni = strtok(NULL, " \n\t");
+	}
+	argk[i] = NULL;
+
+	return (argk);
+}
